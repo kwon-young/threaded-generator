f37b25231002dd3ce7006fa40d7233de69c25818
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2026-02-21 01:24:22 +0100

    refactor: Centralize and lock ThreadedGenerator's thread management

--- threaded_generator.py
+++ threaded_generator.py
@@ -1,6 +1,6 @@
 from typing import Generator, Generic, TypeVar, Iterable
 from queue import Queue, ShutDown
-from threading import Thread
+from threading import Thread, Lock
 
 
 T = TypeVar("T")
@@ -29,7 +29,9 @@ class ThreadedGenerator(Generic[T]):
     def __init__(self, it: Iterable[T], maxsize: int = 1):
         self.it = it
         self.queue: Queue[T] = Queue(maxsize=maxsize)
+        self.thread: Thread | None = None
         self.exception: Exception | None = None
+        self.lock = Lock()
 
     def __repr__(self) -> str:
         return f"ThreadedGenerator({repr(self.it)})"
@@ -45,6 +47,18 @@ class ThreadedGenerator(Generic[T]):
         finally:
             self.queue.shutdown()
 
+    def start(self) -> None:
+        self.queue.is_shutdown = False
+        self.exception = None
+        self.thread = Thread(target=self.run, name=repr(self.it))
+        self.thread.start()
+
+    def enqueue(self) -> Queue[T]:
+        with self.lock:
+            if self.thread is None:
+                self.start()
+        return self.queue
+
     def __iter__(self) -> Generator[T]:
         """
         Iterate over the buffered items.
@@ -53,18 +67,20 @@ class ThreadedGenerator(Generic[T]):
             This method is not thread-safe. Do not call this on the same instance
             from multiple threads.
         """
-        self.queue.is_shutdown = False
-        self.exception = None
-        thread = Thread(target=self.run, name=repr(self.it))
-        thread.start()
-        try:
-            while True:
-                yield self.queue.get()
-        except ShutDown:
-            pass
-        finally:
-            self.queue.shutdown(immediate=True)
-            thread.join()
-            if self.exception:
-                msg = f"Exception from {thread}"
-                raise RuntimeError(msg) from self.exception
+        with self.lock:
+            self.start()
+            try:
+                while True:
+                    yield self.queue.get()
+            except ShutDown:
+                pass
+            finally:
+                self.join()
+
+    def join(self) -> None:
+        self.queue.shutdown(immediate=True)
+        if self.thread:
+            self.thread.join()
+        if self.exception:
+            msg = f"Exception from {self.thread}"
+            raise RuntimeError(msg) from self.exception
